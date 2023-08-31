ed1afb9eb1965a6dfdebd1dcaa97788a6b31cfea
Rishi Doreswamy <rish4862@gmail.com>
2023-08-31 13:51:33 -0500

    update

--- mike.js
+++ mike.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
+const port = (process.env.PORT || 3000)
 
-app.get('/', function (req, res) {
-  res.send('Hi')
+app.set('view engine', 'ejs');
+
+let myVariableServer = 'soft coded server data';
+
+app.get('/rishi', function (req, res) {
+  res.render('index'
+  );
 })
 
-app.listen(3000)
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
+})
+
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
+
+// app.listen(3000)
+
+app.listen(port, () => console.log(`Server is running...on ${ port }` ));
