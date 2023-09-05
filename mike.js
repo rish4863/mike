91bd87d590666105e7aa8bf03cb149091b9c5c87
Rishi Doreswamy <rish4862@gmail.com>
2023-09-05 13:30:32 -0500

    fixed app

--- mike.js
+++ mike.js
@@ -1,21 +1,43 @@
 const express = require('express')
 const app = express()
-const port = (process.env.PORT || 3000)
+const bodyParser = require('body-parser')
+const port = (process.env.PORT || 5000)
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+4
 let myVariableServer = 'soft coded server data';
 
 app.get('/rishi', function (req, res) {
-  res.render('index'
+  res.render('index', 
+  {
+    'myVariableClient' : myVariableServer 
+  }
   );
 })
+
+app.post('/postClientData', function (req, res) {
+  
+   console.log("body: ", req.body)
+   console.log("user Name: ", req.body.userName)
+  //  console.log("params: ", req.params['userName']);
+  
+  // myVariableServer = req.body.userName;
+
+  res.render('index', 
+  {
+    'myVariableClient' : req.body.userName 
+  }
+  );
+})
+
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello World From Express & a PaaS/Render</h1>')
 })
 
-app.get('/index', function (req, res) {
+app.get('/whatever', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 })
 
